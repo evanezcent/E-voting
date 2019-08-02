9e1b272562c1f9b2ad7ad389af253b927747e879
Pratama Yoga <pratamayo16@gmail.com>
2019-08-03 00:06:12 +0700

    add something

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Jalanin router
+app.use('/', usersRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 
